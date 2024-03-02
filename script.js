07ccdbde469b90de7e289889690e3f22c1845eab
maddox1806 <133434338+maddox1806@users.noreply.github.com>
2024-03-02 15:00:03 +0530

    add

---
+++ script.js
@@ -0,0 +1,26 @@
+let div = document.querySelector(".h1");
+let strText = div.textContent;
+div.textContent="";
+let splitText = strText.split("")
+console.log(splitText)
+for(let i = 0; i< splitText.length;i++){
+    div.innerHTML += "<span>" + splitText[i] + "</span>"
+}
+
+let char=0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = div.querySelectorAll('span')[char];
+    span.classList.add('type');
+    char++
+    if(char === splitText.length)
+    {
+        complete();
+        return;
+    }
+}
+function complete(){
+    clearInterval(timer)
+    timer = null;
+}
